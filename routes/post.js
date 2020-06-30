218090b5b75e98c8a0c6db527480406e3cb11f42
Joshua Equere <59533470+marveloper-8@users.noreply.github.com>
2020-06-30 18:47:18 +0100

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -141,7 +141,7 @@ router.put('/unlike',requireLogin,(req,res)=>{
 
 router.put('/comment',requireLogin,(req,res)=>{
     const comment = {
-        const: {textOne, textTwo, textThree} = req.body,
+        text:req.body.text,
         postedBy:req.user._id
     }
     Post.findByIdAndUpdate(req.body.postId,{
